7e648186dfafa1adcfc1378c5e3474b5e09c6268
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2013-01-08 10:35:31 +0000

    Add twitter bootstrap and font awesome

--- grunt.js
+++ grunt.js
@@ -3,10 +3,32 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    lint: {
+      files: ['grunt.js']
+    },
+    concat: {},
+    less: {
+      bootstrap: {
+        options: {
+          paths: ['src/css/bootstrap']
+        },
+
+        src: ['src/css/bootstrap/bootstrap.less'],
+        dest: 'dist/bootstrap.css'
+      }
+    },
+    server: {
+      base: 'dist'
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint'
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
   // Default task(s).
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', 'lint');
 };
