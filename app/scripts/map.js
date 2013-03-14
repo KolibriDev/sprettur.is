3800cec0f785e1ed1bd78cf3bf3e9bdce261aa23
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2013-03-14 16:37:18 +0000

    Lintable map code

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,5 +1,7 @@
 require([], function () {
-    mapbox.load('gudmundur.map-t1y2fg6f', function(o) {
+    'use strict';
+
+    mapbox.load('gudmundur.map-t1y2fg6f', function (o) {
         var map = mapbox.map('map', [o.layer, o.markers], null, [MM.DragHandler()]);
         map.centerzoom(o.center, o.zoom);
 
