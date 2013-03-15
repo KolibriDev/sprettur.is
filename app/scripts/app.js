0cb4be74746958d2f3efc39f6eab3e338cbf7457
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2013-03-15 12:11:47 +0000

    Removed require.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,8 @@
-require(['domready!', 'jquery', 'affix', 'scrollspy', 'tab'], function (doc, $) {
+$(function () {
     'use strict';
 
     var $nav = $('nav');
     $nav.affix({ offset: $nav.offset().top });
-    $nav.scrollspy({ offset: 80 });
 
     /*
     * Tabs
@@ -11,5 +10,17 @@ require(['domready!', 'jquery', 'affix', 'scrollspy', 'tab'], function (doc, $) 
 
     $('a[data-toggle="tab"]').on('shown', function (e) {
         console.log(e);
+    });
+
+    /*
+     * Map
+     */
+    mapbox.load('gudmundur.map-t1y2fg6f', function (o) {
+        var map = mapbox.map('map', [o.layer, o.markers], null, [MM.DragHandler()]);
+        map.centerzoom(o.center, o.zoom);
+
+        if (window.devicePixelRatio > 1) {
+            map.tileSize = { x: 128, y: 128 };
+        }
     });
 });
